759abbcc3fb45047ccfef84102bec73ed0cfdd3c
itso-wavy <you.meet.vvavy@gmail.com>
2023-03-10 18:11:33 +0900

    refactor: 접근성 향상을 위한 마크업 수정

--- js/todolist.js
+++ js/todolist.js
@@ -1,9 +1,9 @@
 // * TodoList
-const TODOS_KEY = 'todos';
-const todoForm = document.querySelector('#todoForm');
-const todoInput = document.querySelector('#todoInput');
-const todoList = document.querySelector('#todoList');
-const addBtn = document.querySelector('#addBtn');
+const TODOS_KEY = "todos";
+const todoForm = document.querySelector("#todoForm");
+const todoInput = document.querySelector("#todoInput");
+const todoList = document.querySelector("#todoList");
+const addBtn = document.querySelector("#addBtn");
 
 let todos = [];
 let counter = 0;
@@ -15,20 +15,24 @@ function saveTodos() {
 function deleteToDo(e) {
   const li = e.target.parentElement;
   li.remove();
-  todos = todos.filter(i => i.id !== parseInt(li.id));
+  todos = todos.filter((i) => i.id !== parseInt(li.id));
   saveTodos();
   counter--;
   addBtn.textContent = counter;
 }
 
 function paintTodo(todo) {
-  const todoItem = document.createElement('li');
-  const delBtn = document.createElement('button');
+  const todoItem = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const delTxt = document.createElement("p");
 
   todoItem.id = todo.id;
   todoItem.innerHTML = `<input type="checkbox" id="todoItem${todo.id}" class="ir todo-item"><label for="todoItem${todo.id}" class="item-label">${todo.txt}</label>`;
-  delBtn.type='button';
-  delBtn.addEventListener('click', deleteToDo);
+  delTxt.textContent = "todo delete";
+  delTxt.classList.add("ir");
+  delBtn.type = "button";
+  delBtn.addEventListener("click", deleteToDo);
+  delBtn.appendChild(delTxt);
   todoItem.appendChild(delBtn);
   todoList.appendChild(todoItem);
   counter++;
@@ -40,10 +44,10 @@ function handleTodoSubmit(e) {
   const newTodo = {
     id: Date.now(),
     txt: todoInput.value,
-  }
+  };
   todos.push(newTodo);
-  todoInput.value = '';
-  todoInput.classList.toggle('ir');
+  todoInput.value = "";
+  todoInput.classList.toggle("ir");
   saveTodos();
   paintTodo(newTodo);
 }
@@ -53,16 +57,16 @@ const savedTodos = localStorage.getItem(TODOS_KEY);
 if (savedTodos) {
   const parsedTodos = JSON.parse(savedTodos);
   todos = parsedTodos;
-  todos.forEach(paintTodo)
+  todos.forEach(paintTodo);
 }
 
-addBtn.addEventListener('mouseover', () => {
-  addBtn.textContent = '+';
-})
-addBtn.addEventListener('mouseout', () => {
+addBtn.addEventListener("mouseover", () => {
+  addBtn.textContent = "+";
+});
+addBtn.addEventListener("mouseout", () => {
   addBtn.textContent = counter;
-})
-addBtn.addEventListener('click', () => {
-  todoInput.classList.toggle('ir');
-})
-todoForm.addEventListener('submit', handleTodoSubmit);
+});
+addBtn.addEventListener("click", () => {
+  todoInput.classList.toggle("ir");
+});
+todoForm.addEventListener("submit", handleTodoSubmit);
